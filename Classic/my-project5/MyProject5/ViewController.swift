2978a1bcd306827042b59666afe1f1884faa54c1
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2021-02-01 20:41:42 -0500

    project5: Setting up

---
+++ Classic/my-project5/MyProject5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyProject5
+//
+//  Created by Viktor Khotimchenko on 2021-02-01.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
