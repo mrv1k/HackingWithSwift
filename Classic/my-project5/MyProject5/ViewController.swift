d305a97610230dc962433eb3e68b21df90641a4e
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2021-02-02 15:59:01 -0500

    project5: wrap up challenges + enum validation

--- Classic/my-project5/MyProject5/ViewController.swift
+++ Classic/my-project5/MyProject5/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class ViewController: UITableViewController {
     var allWords = [String]()
     var usedWords = [String]()
+    var sourceWord = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,22 +20,20 @@ class ViewController: UITableViewController {
             target: self,
             action: #selector(promptForAnswer))
 
+        navigationItem.leftBarButtonItem = UIBarButtonItem(
+            barButtonSystemItem: .refresh,
+            target: self,
+            action: #selector(restart))
+
+
         if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
             if let startWords = try? String(contentsOf: startWordsURL) {
                 allWords = startWords.components(separatedBy: .newlines)
             }
         }
 
-        if allWords.isEmpty {
-            allWords = ["mekmek"]
-        }
+        if allWords.isEmpty { allWords = ["mekmek"] }
         startGame()
-    }
-
-    func startGame() {
-        title = allWords.randomElement()
-        usedWords.removeAll(keepingCapacity: true)
-        tableView.reloadData()
     }
 
     // bugfix
@@ -48,6 +47,19 @@ class ViewController: UITableViewController {
         return cell
     }
 
+    func startGame() {
+        sourceWord = allWords.randomElement() ?? "congests"
+        title = sourceWord
+
+        usedWords.removeAll(keepingCapacity: true)
+        tableView.reloadData()
+    }
+
+    @objc func restart() {
+        startGame()
+    }
+
+
     @objc func promptForAnswer() {
         let ac = UIAlertController(title: "Enter Answer",
                                    message: nil,
@@ -57,52 +69,54 @@ class ViewController: UITableViewController {
         let submitAction = UIAlertAction(
             title: "Submit",
             style: .default) { [weak self, weak ac] _ in
-                guard let answer = ac?.textFields?[0].text else { return }
-                self?.submit(answer)
+                guard let anagramAnswer = ac?.textFields?[0].text else { return }
+                self?.submit(anagramAnswer)
         }
 
         ac.addAction(submitAction)
         present(ac, animated: true)
     }
 
-    func submit(_ rawAnswer: String) {
-        // bugfix
-        guard !rawAnswer.isEmpty else { return }
-        let answer = rawAnswer.lowercased()
+    func submit(_ rawAnagram: String) {
+        let anagram = rawAnagram.lowercased()
 
-        var errorTitle = ""
-        var errorMessage = ""
+        let validation = validate(anagram: anagram)
 
-        // todo refactor to enum
-        if isPossible(word: answer), isOriginal(word: answer), isReal(word: answer) {
-            usedWords.insert(answer, at: 0)
-
-            let indexPath = IndexPath(row: 0, section: 0)
-            tableView.insertRows(at: [indexPath], with: .automatic)
-            return
-        }
-        if !isPossible(word: answer) {
-            errorTitle = "Word not recognized"
-            errorMessage = "You can't just make them up, you know!"
-        } else if !isOriginal(word: answer) {
-            errorTitle = "Word already used"
-            errorMessage = "Be more original!"
-        } else if !isReal(word: answer) {
-            errorTitle = "Word not possible"
-            errorMessage = "You can't spell that world from \(rawAnswer)."
+        guard validation == .valid else {
+            return showErrorMessage(validation: validation)
         }
 
-        let ac = UIAlertController(title: errorTitle, message: errorMessage, preferredStyle: .alert)
+        // passed validation, safe to recort
+        usedWords.insert(anagram, at: 0)
+        let indexPath = IndexPath(row: 0, section: 0)
+        tableView.insertRows(at: [indexPath], with: .automatic)
+    }
+
+    func showErrorMessage(validation: WordValidation) {
+        let ac = UIAlertController(
+            title: validation.error.title, message: validation.error.message, preferredStyle: .alert)
         ac.addAction(UIAlertAction(title: "OK", style: .default))
         present(ac, animated: true)
     }
 
+    func validate(anagram: String) -> WordValidation {
+        guard !anagram.isEmpty else { return .empty }
+        guard anagram.count > 2 else { return .tooShort }
+        guard !sourceWord.starts(with: anagram) else { return .startPart }
+
+        guard isPossible(word: anagram) else { return .notPossible }
+        guard isOriginal(word: anagram) else { return .notOriginal }
+        guard isReal(word: anagram) else { return .notReal(answer: anagram, sourceWord: sourceWord) }
+
+        return .valid
+    }
+
     func isPossible(word: String) -> Bool {
-        guard var tempWord = title?.lowercased() else { return false }
+        var sourceWordCopy = sourceWord
 
         for letter in word {
-            if let position = tempWord.firstIndex(of: letter) {
-                tempWord.remove(at: position)
+            if let position = sourceWordCopy.firstIndex(of: letter) {
+                sourceWordCopy.remove(at: position)
             } else {
                 return false
             }
@@ -117,8 +131,32 @@ class ViewController: UITableViewController {
     func isReal(word: String) -> Bool {
         let checker = UITextChecker()
         let range = NSRange(location: 0, length: word.utf16.count)
-        let misspeledRange = checker.rangeOfMisspelledWord(
+        let misspelledRange = checker.rangeOfMisspelledWord(
             in: word, range: range, startingAt: 0, wrap: false, language: "en")
-        return misspeledRange.location == NSNotFound
+
+        return misspelledRange.location == NSNotFound
+    }
+}
+
+enum WordValidation: Equatable {
+    case empty
+    case tooShort
+    case startPart
+    case notPossible
+    case notOriginal
+    case notReal(answer: String, sourceWord: String)
+    case valid
+
+    var error: (title: String, message: String) {
+        switch self {
+        case .empty: return ("Word is missing", "Cannot be empty :(")
+        case .tooShort: return ("Word is shorter than 3 letters", "Answers that are shorter than three letters.")
+        case .startPart: return ("Word is a starting part", "Not an anagram, just a start part")
+        case .notPossible: return ("Word not possible", "You can't just make them up, you know!")
+        case .notOriginal: return ("Word already used", "Be more original!")
+        case .notReal(let answer, let sourceWord):
+            return ("Word not recognized", "You can't spell \"\(answer)\" from \"\(sourceWord)\".")
+        case .valid: return ("", "")
+        }
     }
 }
