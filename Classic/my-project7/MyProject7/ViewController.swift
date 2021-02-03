3cf9d127e2e0703272b2d01962e60bcbea404150
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2021-02-03 10:04:54 -0500

    project7: setting up

---
+++ Classic/my-project7/MyProject7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyProject7
+//
+//  Created by Viktor Khotimchenko on 2021-02-03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
