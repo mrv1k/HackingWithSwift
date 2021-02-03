82efcda7ad70bc279024ff78decce27f686a64f8
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2021-02-03 10:34:56 -0500

    project7: Parsing JSON using the Codable protocol

---
+++ Classic/my-project7/MyProject7/Petition.swift
@@ -0,0 +1,14 @@
+//
+//  Pettition.swift
+//  MyProject7
+//
+//  Created by Viktor Khotimchenko on 2021-02-03.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
---
+++ Classic/my-project7/MyProject7/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  MyProject7
+//
+//  Created by Viktor Khotimchenko on 2021-02-03.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
