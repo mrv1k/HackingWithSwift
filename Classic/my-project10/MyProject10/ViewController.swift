3154b394b8304495a2dedd9f6a97795d97229205
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2021-02-05 09:12:30 -0500

    project10: setting up

---
+++ Classic/my-project10/MyProject10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyProject10
+//
+//  Created by Viktor Khotimchenko on 2021-02-05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
