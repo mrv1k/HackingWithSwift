ae2ba46ae8198b25e39cb428281ed689cabe292e
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2021-02-05 09:31:45 -0500

    project10: Designing UICollectionView cells

---
+++ Classic/my-project10/MyProject10/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  MyProject10
+//
+//  Created by Viktor Khotimchenko on 2021-02-05.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+
+}
