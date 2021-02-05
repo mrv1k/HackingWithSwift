2d40b614e670559696c204171d93765d02e41796
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2021-02-05 11:44:16 -0500

    project10: Connecting up the people

--- Classic/my-project10/MyProject10/ViewController.swift
+++ Classic/my-project10/MyProject10/ViewController.swift
@@ -19,12 +19,24 @@ class ViewController: UICollectionViewController,
     }
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
+        return people.count
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell
         else { fatalError("Unable to deque reusable cell") }
+
+        let person = people[indexPath.item]
+        cell.name.text = person.name
+
+        let path = getDocumentsDirectory().appendingPathComponent(person.image)
+        cell.imageView.image = UIImage(contentsOfFile: path.path)
+
+        cell.imageView.layer.borderColor = UIColor.systemGray3.cgColor
+        cell.imageView.layer.borderWidth = 2
+        cell.imageView.layer.cornerRadius = 3
+        cell.layer.cornerRadius = 7
+
         return cell
     }
 
@@ -35,7 +47,7 @@ class ViewController: UICollectionViewController,
         present(picker, animated: true)
     }
 
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
         guard let image = info[.editedImage] as? UIImage else { return }
 
         let imageName = UUID().uuidString
@@ -55,5 +67,23 @@ class ViewController: UICollectionViewController,
     func getDocumentsDirectory() -> URL {
         let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
         return paths.first!
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let person = people[indexPath.item]
+
+        let ac = UIAlertController(title: "Rename person", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+
+        ac.addAction(UIAlertAction(title: "OK", style: .default, handler: { [weak self, weak ac] _ in
+            guard let self = self else { return }
+
+            guard let newName = ac?.textFields?.first?.text else { return }
+            person.name = newName
+            self.collectionView.reloadData()
+        }))
+
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        present(ac, animated: true)
     }
 }
