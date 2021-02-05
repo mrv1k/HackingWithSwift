b243fa67f55891e9772c7bb378fd43ca66bef771
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2021-02-05 15:49:19 -0500

    project11: Spinning slots: SKAction

--- Classic/my-project11/MyProject11/GameScene.swift
+++ Classic/my-project11/MyProject11/GameScene.swift
@@ -22,6 +22,11 @@ class GameScene: SKScene {
         makeBouncer(at: CGPoint(x: 512, y: 0))
         makeBouncer(at: CGPoint(x: 768, y: 0))
         makeBouncer(at: CGPoint(x: 1024, y: 0))
+
+        makeSlot(at: CGPoint(x: 128, y: 0), isGood: true)
+        makeSlot(at: CGPoint(x: 384, y: 0), isGood: false)
+        makeSlot(at: CGPoint(x: 640, y: 0), isGood: true)
+        makeSlot(at: CGPoint(x: 896, y: 0), isGood: false)
     }
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -42,5 +47,24 @@ class GameScene: SKScene {
         bouncer.physicsBody = SKPhysicsBody(circleOfRadius: bouncer.size.width / 2.0)
         bouncer.physicsBody?.isDynamic = false
         addChild(bouncer)
+    }
+
+    func makeSlot(at position: CGPoint, isGood: Bool) {
+        let slotBase = isGood
+            ? SKSpriteNode(imageNamed: "slotBaseGood")
+            : SKSpriteNode(imageNamed: "slotBaseBad")
+
+        let slotGlow = isGood
+            ? SKSpriteNode(imageNamed: "slotGlowGood")
+            : SKSpriteNode(imageNamed: "slotGlowBad")
+
+        slotBase.position = position
+        slotGlow.position = position
+        addChild(slotBase)
+        addChild(slotGlow)
+
+        let spin = SKAction.rotate(byAngle: .pi, duration: 10)
+        let spinForever = SKAction.repeatForever(spin)
+        slotGlow.run(spinForever)
     }
 }
