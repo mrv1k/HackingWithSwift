cd7d2ad1f90d5b3149721cb429e5959448653360
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2021-02-07 12:44:46 -0500

    project13: Applying filters: CIContext, CIFilter

--- Classic/my-project13/MyProject13/ViewController.swift
+++ Classic/my-project13/MyProject13/ViewController.swift
@@ -7,18 +7,108 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
     @IBOutlet var imageView: UIImageView!
     @IBOutlet var intensity: UISlider!
+    var currentImage: UIImage!
+
+    var context: CIContext!
+    var currentFilter: CIFilter!
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        title = "YACIFP"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            barButtonSystemItem: .add, target: self, action: #selector(importPicture))
+
+        context = CIContext()
+        currentFilter = CIFilter(name: "CISepiaTone")
     }
 
-    @IBAction func changeFilter(_ sender: Any) {}
+    @objc func importPicture() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }
+        dismiss(animated: true)
+        currentImage = image
+
+        let beginImage = CIImage(image: currentImage)
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+        applyProcessing()
+    }
+
+    @IBAction func changeFilter(_ sender: UIButton) {
+        let filters = ["CIBumpDistortion", "CIGaussianBlur", "CIPixellate", "CISepiaTone",
+                       "CITwirlDistortion", "CIUnsharpMask", "CIVignette"]
+
+        let ac = UIAlertController(title: "Choose Filter", message: nil, preferredStyle: .actionSheet)
+
+        filters.forEach { filter in
+            ac.addAction(UIAlertAction(title: filter, style: .default, handler: setFilter))
+        }
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+
+        if let popoverController = ac.popoverPresentationController {
+            popoverController.sourceView = sender
+            popoverController.sourceRect = sender.bounds
+        }
+
+        present(ac, animated: true)
+    }
+
+    func setFilter(action: UIAlertAction) {
+        guard currentImage != nil else { return }
+        guard let actionTitle = action.title else { return }
+
+        currentFilter = CIFilter(name: actionTitle)
+
+        let beginImage = CIImage(image: currentImage)
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+
+        applyProcessing()
+
+    }
 
     @IBAction func save(_ sender: Any) {}
 
-    @IBAction func intensityChanged(_ sender: Any) {}
+    @IBAction func intensityChanged(_ sender: Any) {
+        applyProcessing()
+    }
+
+    func applyProcessing() {
+        let inputKeys = currentFilter.inputKeys
+
+        if inputKeys.contains(kCIInputIntensityKey) {
+            currentFilter.setValue(intensity.value, forKey: kCIInputIntensityKey)
+        }
+
+        if inputKeys.contains(kCIInputRadiusKey) {
+            currentFilter.setValue(intensity.value * 200, forKey: kCIInputRadiusKey)
+        }
+
+        if inputKeys.contains(kCIInputScaleKey) {
+            currentFilter.setValue(intensity.value * 10, forKey: kCIInputScaleKey)
+        }
+
+        if inputKeys.contains(kCIInputCenterKey) {
+            let vector = CIVector(x: currentImage.size.width / 2, y: currentImage.size.height / 2)
+            currentFilter.setValue(vector, forKey: kCIInputCenterKey)
+        }
+
+
+        guard let outputImage = currentFilter.outputImage else { return }
+
+        if let cgImage = context.createCGImage(
+            outputImage, from: outputImage.extent) {
+            let processedImage = UIImage(cgImage: cgImage)
+            imageView.image = processedImage
+        }
+    }
 }
