10baafe80118211631aac5429f56b16a38203c3d
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2021-02-07 11:19:44 -0500

    project13: setting up

---
+++ Classic/my-project13/MyProject13/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyProject13
+//
+//  Created by Viktor Khotimchenko on 2021-02-07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
