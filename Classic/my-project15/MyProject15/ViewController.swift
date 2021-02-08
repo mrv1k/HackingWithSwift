7e72ea2a839ad9bda48d78292df01c9a5d594aa9
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2021-02-07 22:21:34 -0500

    project15: Transform: CGAffineTransform

--- Classic/my-project15/MyProject15/ViewController.swift
+++ Classic/my-project15/MyProject15/ViewController.swift
@@ -25,9 +25,20 @@ class ViewController: UIViewController {
         UIView.animate(
             withDuration: 1,
             delay: 0,
+            usingSpringWithDamping: 0.5,
+            initialSpringVelocity: 5,
             options: []) {
                 switch self.currentAnimation {
-                case 0: break
+                case 0: self.imageView.transform = CGAffineTransform(scaleX: 2, y: 2)
+                case 2: self.imageView.transform = CGAffineTransform(translationX: -256, y: -256)
+                case 4: self.imageView.transform = CGAffineTransform(rotationAngle: .pi)
+                case 1, 3, 5: self.imageView.transform = .identity
+                case 6:
+                    self.imageView.alpha = 0.1
+                    self.imageView.backgroundColor = .green
+                case 7:
+                    self.imageView.alpha = 1
+                    self.imageView.backgroundColor = .clear
                 default: break
                 }
         } completion: { _ in
