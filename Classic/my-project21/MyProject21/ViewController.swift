0594d8b8ea4c6a8261e4abd3ec0b15e612ef9d5d
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2021-02-10 09:54:47 -0500

    project21: Scheduling notifications

--- Classic/my-project21/MyProject21/ViewController.swift
+++ Classic/my-project21/MyProject21/ViewController.swift
@@ -5,15 +5,46 @@
 //  Created by Viktor Khotimchenko on 2021-02-10.
 //
 
+import UserNotifications
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Register", style: .plain, target: self, action: #selector(registerLocal))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Schedule", style: .plain, target: self, action: #selector(scheduleLocal))
     }
 
+    @objc func registerLocal() {
+        let center = UNUserNotificationCenter.current()
 
+        center.requestAuthorization(options: [.alert, .badge, .sound]) { (granted, error) in
+            if granted {
+                print("Yay :)")
+            } else {
+                print("Nay :(")
+            }
+        }
+    }
+
+    @objc func scheduleLocal() {
+        let center = UNUserNotificationCenter.current()
+
+        let content = UNMutableNotificationContent()
+        content.title = "Late wake up call"
+        content.body = "The early bird cathes the worm, but the second mouse gets the cheese."
+        content.categoryIdentifier = "alarm"
+        content.sound = .default
+
+        var dateComponent = DateComponents()
+        dateComponent.hour = 10
+        dateComponent.minute = 30
+//        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponent, repeats: true)
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        center.add(request)
+    }
 }
 
