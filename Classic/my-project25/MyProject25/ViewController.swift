7fe394882be42cea0675b9f37ad25e8405540122
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2021-02-11 14:33:33 -0500

    project25: Going peer to peer

--- Classic/my-project25/MyProject25/ViewController.swift
+++ Classic/my-project25/MyProject25/ViewController.swift
@@ -5,16 +5,29 @@
 //  Created by Viktor Khotimchenko on 2021-02-11.
 //
 
+import MultipeerConnectivity
 import UIKit
 
-class ViewController: UICollectionViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
+typealias ImagePickerProtocols = UIImagePickerControllerDelegate & UINavigationControllerDelegate
+//typealias MCProtocols =
+
+class ViewController: UICollectionViewController, ImagePickerProtocols, MCSessionDelegate, MCBrowserViewControllerDelegate {
     var images = [UIImage]()
+
+    var peerID = MCPeerID(displayName: UIDevice.current.name)
+    var mcSession: MCSession?
+    var mcAdvertiserAssistant: MCAdvertiserAssistant?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         title = "Selfie Share"
+
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(showConnectionPrompt))
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .camera, target: self, action: #selector(importPicture))
+
+        mcSession = MCSession(peer: peerID, securityIdentity: [], encryptionPreference: .required)
+        mcSession?.delegate = self
     }
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -45,5 +58,27 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
 
         images.insert(image, at: 0)
         collectionView.reloadData()
+    }
+
+    @objc func showConnectionPrompt() {
+        let ac = UIAlertController(title: "Connect to others", message: nil, preferredStyle: .alert)
+
+        ac.addAction(UIAlertAction(title: "Host a session", style: .default, handler: startHosting))
+        ac.addAction(UIAlertAction(title: "Join a session", style: .default, handler: joinSession))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+
+        present(ac, animated: true)
+    }
+
+    func startHosting(action: UIAlertAction) {
+        guard let mcSession = mcSession else { return }
+        mcAdvertiserAssistant = MCAdvertiserAssistant(serviceType: "my-hws-project25", discoveryInfo: nil, session: mcSession)
+    }
+
+    func joinSession(action: UIAlertAction) {
+        guard let mcSession = mcSession else { return }
+        let mcBrowser = MCBrowserViewController(serviceType: "my-hws-project25", session: mcSession)
+        mcBrowser.delegate = self
+        present(mcBrowser, animated: true)
     }
 }
