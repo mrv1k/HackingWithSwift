31352e18df5e7bdd605ed86c834780fa00c00f67
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2021-02-12 12:03:24 -0500

    project27: Creating the sandbox

--- Classic/my-project27/MyProject27/ViewController.swift
+++ Classic/my-project27/MyProject27/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    var currenDrawType = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        drawRectangle()
     }
 
+    @IBAction func onRedrawButtonTap(_ sender: UIButton) {
+        currenDrawType += 1
 
+        if currenDrawType > 5 {
+            currenDrawType = 0
+        }
+
+        switch currenDrawType {
+        case 0:
+            drawRectangle()
+        default:
+            break
+        }
+    }
+
+    func drawRectangle() {}
 }
-
